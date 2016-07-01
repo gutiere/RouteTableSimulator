b6a63768110a482f9622dfae3500001c3bca5752
Edgardo Gutierrez Jr <EdgardoGutierrezJr@Gmail.com>
2016-07-01 13:20:03 -0700

    Added some guy stuff if we went to try

--- RoutingTable.java
+++ RoutingTable.java
@@ -1,20 +1,38 @@
 import java.util.ArrayList;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+import java.awt.Dimension;
 
 public class RoutingTable extends JPanel {
-    private static ArrayList<Route> myRoutes;
+    private  ArrayList<Route> myRoutes;
+    private GridLayout myGrid;
+    private JScrollPane myScroller;
+    private JLabel myLabel;
 
     public RoutingTable() {
         init();
     }
 
-    private static void init() {
+    private void init() {
         myRoutes = new ArrayList<Route>();
         myRoutes.add(new Route("0.0.0.0       ", "E1", myRoutes.size() + 1));
+
+
+
+        // GUI stuff
+        
+        this.setPreferredSize(new Dimension(600, 400));
+        myGrid = new GridLayout(2, 6);
+        myLabel = new JLabel();
+        myScroller = new JScrollPane(myLabel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        myScroller.setSize(new Dimension(600, 400));
+        this.add(myScroller);
     }
 
 
-    public static void run() {
+    public void run() {
         for (int i = 0; i < 1000; i++) {
 
 
@@ -35,7 +53,19 @@ public class RoutingTable extends JPanel {
     }
 
 
-    private static void print() {
+    private void print() {
+        // GUI stuff
+
+        // StringBuilder sb = new StringBuilder();
+        // sb.append("<html>");
+        // sb.append("<br>Seq # \t\t Time \t\t Destination \t       Metric    Port     Device<br>");
+        // for (Route r : myRoutes) {
+        //     sb.append(r + "<br>");
+        // }
+        // sb.append("</html>");
+        // myLabel.setText(sb.toString());
+
+
         System.out.println("\nSeq # \t\t Time \t\t Destination \t       Metric    Port     Device");
         for (Route r : myRoutes) {
             System.out.println(r);
