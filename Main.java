886cefcfaf5e948c7f74a7169db1efbdde56afaa
ssarai <ssarai@uw.edu>
2016-07-02 21:53:18 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+/**
+ * Main class to run our route table by creating a route object.
+ */
+
 public class Main {
     public static void main(String[] theArgs) {
         RoutingTable myTable = new RoutingTable();
